c89837c717d054384c080ccb06ead7b28b3aaf7c
AbrahamCHL <abrahamchl93@gmail.com>
2020-10-21 12:23:52 +0200

    Ejercicio bus clase terminado

--- ejercicios/python/Bus.py
+++ ejercicios/python/Bus.py
@@ -1,11 +1,7 @@
 class Bus():
     def __init__(self,numeroPlazas):
-        self.setNumeroPlazas(numeroPlazas)
+        self.numeroplazas = numeroPlazas
         self.plazas_disponibles = numeroPlazas
-
-    
-    def getPlazasDisponibles(self):
-        return self.plazas_disponibles
 
     def getNumeroPlazas(self):
         return self.numeroplazas
@@ -13,17 +9,31 @@ class Bus():
     def getPlazasDisponibles(self):
         return self.plazas_disponibles
     
-    def setNumeroPlazas(self,nPlazas):
-        self.numeroplazas = nPlazas
-    
     def ventaDeBilletes(self,billetesAcomprar):
-        self.plazas_disponibles -= billetesAcomprar
+        ventacorrecta = False
+        if billetesAcomprar > self.getPlazasDisponibles():
+            #ventacorrecta = False
+            pass
         
+        else:
+            self.plazas_disponibles -= billetesAcomprar
+            ventacorrecta = True
 
+        return ventacorrecta
+        
     def devolucion(self,BilletesADevolver):
-        self.plazas_disponibles += BilletesADevolver
-
+        devolucioncorrecta = False
+        if self.getNumeroPlazas() < (BilletesADevolver + self.getPlazasDisponibles()):
+            #devolucioncorrecta = False
+            pass
+        
+        else:
+            self.plazas_disponibles += BilletesADevolver
+            devolucioncorrecta = True
+        return devolucioncorrecta
     
     def billetesVendidos(self):
         self.billetes_vendidos = self.getNumeroPlazas() - self.getPlazasDisponibles()
         return self.billetes_vendidos
+
+
