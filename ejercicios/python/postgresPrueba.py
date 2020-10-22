0fb045a2081f6875a1c7e6d2f0c282e9ba6667c1
AbrahamCHL <abrahamchl93@gmail.com>
2020-10-23 00:02:39 +0200

    Postgres prueba

---
+++ ejercicios/python/postgresPrueba.py
@@ -0,0 +1,27 @@
+import psycopg2
+from tkinter import *
+
+
+root = Tk()
+
+ 
+root.title("Hola mundo")     
+root.resizable(0, 150) 
+root.geometry("120x200")
+
+conn = psycopg2.connect(host="localhost", port = 5432, database="postgres", user="postgres", password="postgres")
+
+
+cur = conn.cursor()
+
+cur.execute("""SELECT col1, col2, col3 FROM public.hola""")
+query_results = cur.fetchall()
+print(query_results)
+x = 5
+label = Label(root, text= query_results)
+label.pack() 
+
+
+root.mainloop() 
+cur.close()
+conn.close()
